284756ba8dd7c4334e39c47d8f4ac1d6c69342dc
Konstantin Ryzhikh <k.ryzhikh@gmail.com>
2022-07-03 23:32:04 +0300

    Initial Commit

---
+++ WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by kostya on 03.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
